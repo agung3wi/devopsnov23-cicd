88e1cbbaa7fa7022cc445b22b6614a57b0dc7752
Agung <agungskripsi05@gmail.com>
2024-01-04 10:27:55 +0700

    1.2

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 });
 
 Route::get('/version', function () {
-    return ["version" => "1.1"];
+    return ["version" => "1.2"];
 });
 
 Route::get('/lang', function () {
